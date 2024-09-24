259dc0194ecc3f8d753862f0117a161972d56a28
Nicoly <nicoly.basso@escola.pr.gov.br>
2024-09-24 09:27:24 -0300

    iniciando nosso repositorio

---
+++ src/classeprincipal.java
@@ -0,0 +1,11 @@
+public class classeprincipal {
+    public static void main(String[] args) {
+        //criação de objetos
+        Pessoa pessoa = new Pessoa("João", 30);
+        Carro carro = new carro("Toyota", "Corolla");
+        //métodos
+        pessoa.mostrar Detalhes ();
+        carro.mostrar Detalhes ();
+    }
+    }
+}
---
+++ src/secundaria.java
@@ -0,0 +1,10 @@
+public class secundaria {
+    class Pessoa{
+        Private String nome;
+        Private int idade;
+        Public Pessoa (String nome, int idade){
+            this.nome = nome;
+            this.idade = idade;
+        }
+    }
+}
